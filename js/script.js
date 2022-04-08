2a84e758c5321834df37e5b0f579aac3e8b4a52c
expldec <explosive.deconstruction@gmail.com>
2022-04-08 16:18:25 +0200

    Initial implementation of requirements

--- js/script.js
+++ js/script.js
@@ -1,53 +1,111 @@
-// Settiamo gli input richiesti e tutti i valori dati dalla consegna
-// la percentuale di sconto l'ho scritta in forma decimale (es. 40% -> 0.40) per facilità di calcolo.
+//imbrigliamo i bottoni 
 
-const passengerKM = prompt("How many kilometers does the passenger need to travel?");
-const passengerAge = prompt("What is the age of the passenger?");
+const generateButton = document.getElementById("btn-generate");
+const cancelButton = document.getElementById("btn-cancel");
+
+//dichiariamo le varaibili della nostra "business logic"
 const ratePerKM = 0.21;
 const underageDiscount = 0.20;
-const over65Discount = 0.40;
+const seniorDiscount = 0.40;
+const underageDiscountName = "Sconto Minorenni";
+const seniorDiscountName = "Sconto Silver";
+const noDiscountName = "Biglietto Standard";
 
-// calcoliamo il prezzo del biglietto al lordo di eventuali sconti
-const grossTicketPrice = passengerKM * ratePerKM;
-// inizializziamo una variabile che conterrà il prezzo finale al netto degli sconti
-let ticketPrice;
-// inizializziamo una variabile stringa che ci servirà per mostrare sulla pagina le informazioni extra
-// sul calcolo del prezzo finale del biglietto
-let breakdownExtra = '';
 
-// controlliamo se il passeggero rientra in un'età che ha diritto a uno sconto 
-if (passengerAge >= 65) {
-    // per ottenere il prezzo finale, moltiplichiamo il prezzo lordo per (1 meno lo sconto decimale)
-    // e lo convertiamo in un numero a 2 cifre decimali
-    ticketPrice = (grossTicketPrice * (1 - over65Discount)).toFixed(2);
-    // aggiungiamo dell'HTML che scriverà sulla pagina i calcoli intermedi.
-    breakdownExtra = `<br> Over 65 discount (-${over65Discount * 100}%): <strong>-€${(grossTicketPrice * over65Discount).toFixed(2)}</strong><br>
-    Final price: <strong>€${ticketPrice}</strong>`;
-} 
-// come sopra, ma per gli under18
-else if (passengerAge < 18){
-    ticketPrice = (grossTicketPrice * (1 - underageDiscount)).toFixed(2);
-    breakdownExtra = `<br> Under 18 discount (-${underageDiscount * 100}%): <strong>-€${(grossTicketPrice * underageDiscount).toFixed(2)}</strong><br>
-    Final price: <strong>€${ticketPrice}</strong>`;
+function clickGenerate() {
+    //preleviamo i dati del form
+    const passengerName = document.getElementById("passenger-name").value;
+    const passengerKM = document.getElementById("passenger-km").value;
+    const passengerAgeRange = document.getElementById("passenger-age-range").value;
+    const grossTicketPrice = passengerKM * ratePerKM;
+
+    let ticketPrice;
+    let ticketRate;
+
+
+    //determiniamo se c'è uno sconto applicabile con uno switch statement sul passengerAgeRange,
+    //calcoliamo il ticketPrice finale formattandolo come stringa.
+    //assegnamo anche il nome dell'offerta che mostreremo sul biglietto (ticketRate)
+    switch (passengerAgeRange) {
+        case "minor":
+            ticketPrice = (grossTicketPrice * (1 - underageDiscount)).toFixed(2);
+            ticketRate = underageDiscountName;
+            break;
+        case "senior":
+            ticketPrice = (grossTicketPrice * (1 - seniorDiscount)).toFixed(2);
+            ticketRate = seniorDiscountName;
+            break;
+        default:
+            ticketPrice = grossTicketPrice.toFixed(2);
+            ticketRate = noDiscountName;
+            break;
+    }
+
+    //assegnamo un numero di carrozza a caso da 1 a 10
+    const ticketCar = Math.floor(Math.random() * 10) + 1;
+
+    //assegnamo un numero casuale da 90000 a 99999 come "codice CP"
+    const ticketCode = Math.floor(Math.random() * 10000) + 90000;
+
+    //scriviamo i nostri dati nei punti giusti del DOM 
+    document.querySelector(".ticket-passenger p").innerHTML = passengerName;
+    document.querySelector(".ticket-rate p").innerHTML = ticketRate;
+    document.querySelector(".ticket-car p").innerHTML = ticketCar;
+    document.querySelector(".ticket-code p").innerHTML = ticketCode;
+    document.querySelector(".ticket-price p").innerHTML = `€ ${ticketPrice}`;
+
+    //rendiamo visibile il div con le informazioni sul biglietto
+    document.querySelector(".ticket-list").classList.remove("d-none");
 }
-// se il passeggero non ha diritto allo sconto, il prezzo finale è uguale al prezzo lordo.
-// questo è anche il momento di convertire il prezzo finale in una stringa con 2 decimali 
-else {
-    ticketPrice = grossTicketPrice.toFixed(2);
-}
 
-// inizializziamo la variabile che contiene l'HTML del titolo che conterrà in sintesi i dati forniti e il prezzo finale
-let message = `
-The price of a ${passengerKM}-kilometer ticket for a ${passengerAge}-year old is €${ticketPrice}
-`;
 
-// inizializziamo anche una variabile contenente l'HTML da mstrare sotto il titolo,
-// che elenca nel dettaglio i dati forniti, il risultato del calcolo,
-// e eventuali calcoli extra per gli aventi diritto allo sconto (breakdownExtra).
-let breakdown = `
-Base rate per KM: <strong>€${ratePerKM}</strong> <br>
-KM to travel: <strong>${passengerKM}</strong> <br>
-Ticket price: <strong>€${grossTicketPrice.toFixed(2)}</strong>
-${breakdownExtra}`;
 
-// piazziamo le nostre variabili nella pagina
+
+generateButton.addEventListener("click", clickGenerate);
+
+
+cancelButton.addEventListener("click",
+    function cancelTicket() {
+        document.querySelector(".ticket-list").classList.add("d-none");
+        document.getElementById("passenger-name").value = '';
+        document.getElementById("passenger-km").value = '';
+        document.getElementById("passenger-age-range").value = '';
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+// 
+// let breakdownExtra = '';
+
+// if (passengerAge >= 65) {
+//     ticketPrice = (grossTicketPrice * (1 - seniorDiscount)).toFixed(2);
+//     breakdownExtra = `<br> Over 65 discount (-${seniorDiscount * 100}%): <strong>-€${(grossTicketPrice * seniorDiscount).toFixed(2)}</strong><br>
+//     Final price: <strong>€${ticketPrice}</strong>`;
+// } 
+// else if (passengerAge < 18){
+//     ticketPrice = (grossTicketPrice * (1 - underageDiscount)).toFixed(2);
+//     breakdownExtra = `<br> Under 18 discount (-${underageDiscount * 100}%): <strong>-€${(grossTicketPrice * underageDiscount).toFixed(2)}</strong><br>
+//     Final price: <strong>€${ticketPrice}</strong>`;
+// }
+// else {
+//     ticketPrice = grossTicketPrice.toFixed(2);
+// }
+
+// let message = `
+// The price of a ${passengerKM}-kilometer ticket for a ${passengerAge}-year old is €${ticketPrice}
+// `;
+
+// let breakdown = `
+// Base rate per KM: <strong>€${ratePerKM}</strong> <br>
+// KM to travel: <strong>${passengerKM}</strong> <br>
+// Ticket price: <strong>€${grossTicketPrice.toFixed(2)}</strong>
+// ${breakdownExtra}`;
